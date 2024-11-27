8cc27c97945706e700551320def9b4128a6b8500
hamdi zaghouan <141347852+hamdiz0@users.noreply.github.com>
2024-11-27 23:06:54 +0100

    Update install-minikube.sh

--- install-minikube.sh
+++ install-minikube.sh
@@ -1,3 +1,11 @@
+# install docker
+curl -fsSL https://test.docker.com -o test-docker.sh
+sudo sh test-docker.sh
+
+# make docker commands available for the current user
+sudo usermod -aG docker $(whoami)
+newgrp docker
+
 # install minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
@@ -6,16 +14,7 @@ sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-a
 echo 'alias kubectl="minikube kubectl --"' | sudo tee -a /etc/bash.bashrc > /dev/null
 
 # strat minikube as a docker container
-
 minikube start --driver=docker
 
-# install docker
-
-curl -fsSL https://test.docker.com -o test-docker.sh
-sudo sh test-docker.sh
-
-# make docker commands available for the current user
-sudo usermod -aG docker $(whoami)
-newgrp docker
-
 echo "minikube installed"
+
